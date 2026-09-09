2b1471072a85a3bc020df5e917e1a0d5e9b0c0cc
martin1890 <36193985+martin1890@users.noreply.github.com>
2026-09-09 22:16:34 +0200

    nu har vi fixat get och set :D

--- lab1/main.c
+++ lab1/main.c
@@ -5,8 +5,8 @@
 
 
 typedef struct TreeNode {
-    uint32_t max_in_subtree;
-    uint32_t value;  // exists if leaf (somewhere)
+    int max_in_subtree;
+    int value;  // exists if leaf (somewhere)
     bool has_left;
     bool has_right;
     struct TreeNode* left;
@@ -15,7 +15,7 @@ typedef struct TreeNode {
 
 typedef struct Tree {
     TreeNode* root;
-    uint32_t height;
+    int height;
 } Tree;
 
 typedef struct StackNode {
@@ -46,90 +46,133 @@ void pop(Stack* stack) {
 }
 
 Stack new_array() {
-    Tree tree;
-    Stack stack;
-    push(&stack, &tree);
+    Stack stack = {
+        .top = NULL,
+        .count = 0
+    };
+
+    Tree* tree = malloc(sizeof(Tree));
+    tree->root = NULL;
+    tree->height = 0;
+
+    push(&stack, tree);
+
     return stack;
 }
 
-int navigate_tree(uint32_t height, uint32_t index) {
-    for (uint32_t i = height-1; i >= 0; i--) {
-        uint32_t bit = (index >> i) & 1;
-        if (bit == 0) {
-            printf("Left\n");
-        } else {
-            printf("Right\n");
-        }
-    }
-}
-
-
-TreeNode* new_tree_node(bool root, uint32_t value, uint32_t max_in_subtree, TreeNode* current_node) {
-
+TreeNode* copy_node(TreeNode* current_node) {
     TreeNode* new_node = malloc(sizeof(TreeNode));
-    new_node->max_in_subtree = max_in_subtree;
-    new_node->value = value;
-    new_node->has_left = false;
-    new_node->has_right = false;
-    new_node->left = NULL;
-    new_node->right = NULL;
 
-    if (current_node->has_left) {
-        new_node->left = current_node->left;
-        new_node->has_left = true;
+    if (current_node == NULL) {
+        new_node->max_in_subtree = 0;
+        new_node->value = 0;
+        new_node->has_left = false;
+        new_node->has_right = false;
+        new_node->left = NULL;
+        new_node->right = NULL;
+        return new_node;
     }
-    if (current_node->has_right) {
-        new_node->right = current_node->right;
-        new_node->has_right = true;
-    }
+
+    *new_node = *current_node;
     return new_node;
 }
 
-TreeNode* follow_or_depart_old_tree(TreeNode* parent_node, bool is_left, int value) {
 
-    if (is_left) {
-        if (parent_node->has_left) {
-        uint32_t max_in_sub_tree = parent_node->left->max_in_subtree;
-        if (max_in_sub_tree < value) {
-            TreeNode* next_tree_node = new_tree_node(parent_node, is_left, value);
-            return next_tree_node;
-        }
-        } else {
-            TreeNode* next_tree_node = new_tree_node(parent_node, is_left, value);
-            return next_tree_node;
-        }
-    } else if (parent_node->has_right) {
-        uint32_t max_in_sub_tree = parent_node->right->max_in_subtree;
-    } else {
-        new_tree_node(parent_node, is_left);
+void update_max_in_subtree(TreeNode* updated_node) {
+
+    int left_max = 0;
+    int right_max = 0;
+
+    if (updated_node->has_left) {
+        left_max = updated_node->left->max_in_subtree;
     }
-
+    if (updated_node->has_right) {
+        right_max = updated_node->right->max_in_subtree;
+    }
+    updated_node->max_in_subtree = max(left_max, right_max);
 }
 
+TreeNode* extend_tree_height(TreeNode* root, int old_height, int new_height) {
+    while (old_height < new_height) {
+        TreeNode* new_root = copy_node(NULL);
 
-void recursive_set(Stack* stack, TreeNode* updated_tree_node, TreeNode* current_node, int i, int level, int height, int value) {
+        new_root->left = root;
 
-    
+        if (root != NULL) {
+            new_root->has_left = true;
+            new_root->max_in_subtree = root->max_in_subtree;
+        }
 
+        root = new_root;
+        old_height++;
+    }
+    return root;
+}
+
+TreeNode* recursive_set(TreeNode* current_node, int i, int level, int height, int value) {
+
+    TreeNode* updated_node = copy_node(current_node);
+
+    if (height == level) {
+        updated_node->value = value;
+        updated_node->max_in_subtree = value;
+        return updated_node;
+    }
+
+    bool go_right = (i >> (height - level - 1)) & 1;
+    if (go_right) {
+        updated_node->right = recursive_set(current_node ? current_node->right: NULL, i, level + 1, height, value);
+        updated_node->has_right = true;
+    }
+    else {
+        updated_node->left = recursive_set(current_node ? current_node->left: NULL, i, level + 1, height, value);
+        updated_node->has_left = true;
+    }
+
+    update_max_in_subtree(updated_node);
+
+    return updated_node;
 }
 
 void set(Stack* stack, int i, int value) {
-
+    Tree* current_tree = stack->top->tree;
     Tree* updated_tree = malloc(sizeof(Tree));
-    uint32_t new_height = max(31 - __builtin_clz(i), stack->top->tree->height);
-    updated_tree->height = new_height;
-    TreeNode* current_node = stack->top->tree->root;
-    TreeNode* updated_tree_root;
-    if (value > current_node->max_in_subtree) {
-        updated_tree_root = new_tree_node(true, 0, value, current_node);
-        updated_tree->root = updated_tree_root;
-    } else {
-        updated_tree->root = current_node;
-        updated_tree_root = current_node;
-    }
-    recursive_set(stack, updated_tree_root, current_node, i, 1, new_height, value);
+
+    int required_height;
+
+    if (i == 0) {required_height = 1;
+    } else {required_height = 32 - __builtin_clz((int)i);}
+
+    required_height = max(required_height, current_tree->height);
+    updated_tree->height = required_height;
+
+    TreeNode* root = extend_tree_height(current_tree->root, current_tree->height, required_height);
+
+    updated_tree->root = recursive_set(root, i, 0, required_height, value);
+    push(stack, updated_tree);
 }
 
+void get(Stack* stack, int i) {
+    Tree* current_tree = stack->top->tree;
+    TreeNode* current_node = current_tree->root;
+    int height = current_tree->height;
+    for (int j = height-2; j >= 0; j--) {
+        int bit = (i >> j) & 1;
+        if (bit == 0 && current_node->has_left) {
+            current_node = current_node->left;
+        } else if (bit == 1 && current_node->has_right) {
+            current_node = current_node->right;
+        } else {
+            printf("0\n");
+            return;
+        }
+    }
+    if (current_node->value == NULL) {
+        printf("0\n");
+    } else {
+        printf("%u\n", current_node->value);
+    }
+}
 
 int main() {
     // Your code here
